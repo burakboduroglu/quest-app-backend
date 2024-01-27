24abb43aa8d69c075a1c39109ac780c64a8bc0aa
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-01-27 18:49:28 +0300

    FEAT: PostResponse class added

--- src/main/java/com/example/questappbackend/controllers/PostController.java
+++ src/main/java/com/example/questappbackend/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.example.questappbackend.controllers;
 import com.example.questappbackend.entities.Post;
 import com.example.questappbackend.requests.PostCreateRequest;
 import com.example.questappbackend.requests.PostUpdateRequest;
+import com.example.questappbackend.responses.PostResponse;
 import com.example.questappbackend.services.PostService;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,7 @@ public class PostController {
     }
 
     @GetMapping
-    public List<Post> getAllPosts(@RequestParam Optional<Long> userId){
+    public List<PostResponse> getAllPosts(@RequestParam Optional<Long> userId){
         return postService.getAllPosts(userId);
     }
 
--- src/main/java/com/example/questappbackend/services/PostService.java
+++ src/main/java/com/example/questappbackend/services/PostService.java
@@ -5,10 +5,12 @@ import com.example.questappbackend.entities.User;
 import com.example.questappbackend.repos.PostRepository;
 import com.example.questappbackend.requests.PostCreateRequest;
 import com.example.questappbackend.requests.PostUpdateRequest;
+import com.example.questappbackend.responses.PostResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
@@ -20,11 +22,14 @@ public class PostService {
         this.userService = userService;
     }
 
-    public List<Post> getAllPosts(Optional<Long> userId) {
+    public List<PostResponse> getAllPosts(Optional<Long> userId) {
+        List<Post> list;
         if(userId.isPresent()){
-            return postRepository.findByUserId(userId.get());
+            list = postRepository.findByUserId(userId.get());
+        } else {
+            list = postRepository.findAll();
         }
-        return postRepository.findAll();
+        return list.stream().map(p  -> new PostResponse(p)).collect(Collectors.toList());
     }
 
     public Post getOnePostById(Long postId) {
