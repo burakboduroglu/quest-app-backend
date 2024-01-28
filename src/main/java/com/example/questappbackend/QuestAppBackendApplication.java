7df429694162f9cd1e33958a224e4e4db639102b
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-01-28 18:43:43 +0300

    Add .vscode/ to .gitignore

--- src/main/java/com/example/questappbackend/QuestAppBackendApplication.java
+++ src/main/java/com/example/questappbackend/QuestAppBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuestAppBackendApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(QuestAppBackendApplication.class, args);
     }
