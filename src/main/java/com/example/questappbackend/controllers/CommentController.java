2c8e1611d60063ed689a2022b0702b688546f884
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-01-31 13:30:49 +0300

    Comment

--- src/main/java/com/example/questappbackend/controllers/CommentController.java
+++ src/main/java/com/example/questappbackend/controllers/CommentController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/comments")
 public class CommentController {
     private CommentService commentService;
